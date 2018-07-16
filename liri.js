a7684568fa20731c32f55493f0735d2b4fdcde6d
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-07-16 09:37:19 -0700

    planning & file setup

---
+++ liri.js
@@ -0,0 +1,16 @@
+conts liribot = require("./liribot.js");
+
+// set default inputs
+
+// set first input var (if present)
+
+// Store the rest of the inputs in a single string "data" (if present)
+
+// Switch for different commands
+/*
+    my-tweets           -   liribot.myTweets();
+    spotify-this-song   -   liribot.spotifyThisSong(data);
+    movie-this          -   liribot.movieThis(data);
+    do-what-it-says     -   liribot.doWhatItSays();
+*/
+
---
+++ liribot.js
@@ -0,0 +1,14 @@
+// just creating and exporting a utility type object with API functions
+
+// require all API modules
+
+// require keys file
+const keys = require("./keys.js")
+
+// functions required in the liribot export
+/*
+    liribot.myTweets();
+    liribot.spotifyThisSong(data);
+    liribot.movieThis(data);
+    liribot.doWhatItSays();
+*/
